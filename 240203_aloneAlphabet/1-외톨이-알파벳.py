63badc05a366d7855adfab4b424e486152598398
Yelim Koo <46446403+debbs061@users.noreply.github.com>
2024-02-03 16:54:17 +0900

    Add files via upload

---
+++ 240203_aloneAlphabet/1-외톨이-알파벳.py
@@ -0,0 +1,18 @@
+def solution(input_string):
+    if input_string == '':
+        return "N"
+
+    # 문자열에서 등장하는 알파벳을 담는 set
+    alpha_set = set(input_string[0])
+
+    # 외톨이를 담는 set
+    loner_set = set()
+    for i in range(1, len(input_string)):
+        # 직전에 등장한 알파벳과 다른 알파벳인데, 알파벳 set 에 이미 존재하면 외톨이라는 뜻
+        if input_string[i - 1] != input_string[i] and input_string[i] in alpha_set:
+            loner_set.add(input_string[i])
+
+        # set 은 중복 처리가 되므로 loop 를 도는 동안 무조건 담는다.
+        alpha_set.add(input_string[i])
+
+    return ''.join(sorted(loner_set)) if len(loner_set) != 0 else "N"
